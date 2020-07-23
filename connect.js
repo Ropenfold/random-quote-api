2163c0c8d96a252e54406ab16382e6e2f0858180
Stuart Roper <stuart.roper@bravissimo.com>
2020-07-23 20:22:31 +0100

    add catch statement to connect.js

--- connect.js
+++ connect.js
@@ -2,11 +2,13 @@ const mongodb = require('mongodb');
 
 const uri = process.env.MONGODB_URI
 
-const connect = database => { 
+ const connect = database => { 
     return mongodb.MongoClient.connect(uri, {useNewUrlParser: true})
     .then(client => {
         console.log("connected to MongoDB!")
         return client.db(database)
+    }).catch((err) => {
+        console.log(err)
     })
 };
 
