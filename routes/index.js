215ec7a6df1b435bd79142fbae212811e4cfe123
Stuart Roper <stuart.roper@bravissimo.com>
2020-07-23 15:12:53 +0100

    update main index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const connect = require('../connect');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
+var db;
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  db.collection("quotes")
+  .aggregate([{ $sample: { size: 1 } }])
+  .toArray()
+  .then(data => res.json({payload: data}));
 });
+
+connect("quotes").then(database => {
+  db = database;
+})
 
 module.exports = router;
