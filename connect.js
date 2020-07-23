363d8031dd8a329d6e929cc2dffe06eaf584d6cb
Stuart Roper <stuart.roper@bravissimo.com>
2020-07-23 20:35:19 +0100

    add console.log

--- connect.js
+++ connect.js
@@ -6,6 +6,7 @@ const uri = process.env.MONGODB_URI
     return mongodb.MongoClient.connect(uri, {useNewUrlParser: true})
     .then(client => {
         console.log("connected to MongoDB!")
+        console.log("process.env.MONGODB_URI", process.env.MONGODB_URI);
         return client.db(database)
     }).catch((err) => {
         console.log(err)
