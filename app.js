46df60da51e65813b1f3d4c1c34d58aab10eb998
Stuart Roper <stuart.roper@bravissimo.com>
2020-07-30 19:50:27 +0100

    add cors

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-require('dotenv').config();
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var randomRouter = require('./routes/random');
-var port = '3000';
+var indexRouter = require("./routes/index");
+var randomRouter = require("./routes/random");
+var port = "3000";
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/random', randomRouter);
+app.use("/", indexRouter);
+app.use("/random", randomRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json('error');
+  res.json("error");
 });
 
 module.exports = app;
